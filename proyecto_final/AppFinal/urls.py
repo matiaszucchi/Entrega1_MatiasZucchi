e20badc1384334021d3ec12387d694942e7d24cb
matiaszucchi <matiaszucchi@gmail.com>
2022-09-03 15:41:49 -0300

    tercera subida

--- proyecto_final/AppFinal/urls.py
+++ proyecto_final/AppFinal/urls.py
@@ -4,7 +4,7 @@ from AppFinal.views import *
 urlpatterns = [
 
     path('', inicio, name='AppFinalInicio'),
-    path('monotribustista/', monotributista, name='AppFinalMonotributista'),
+    path('monotributista/', monotributista, name='AppFinalMonotributista'),
     path('responsableinscripto/', responsable_inscripto, name='AppFinalResponsable'),
     path('impuesto/', impuestos, name='AppFinalImpuesto'),
     path('formulario/', formulario, name='AppFinalFormulario'),
